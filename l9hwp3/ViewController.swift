6e9e7f9356b7a3a475ae460adaa46a23a3d52ae3
nik1olay <85620548+nik1olay@users.noreply.github.com>
2021-08-18 12:44:57 +0300

    Initial Commit

---
+++ l9hwp3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  l9hwp3
+//
+//  Created by Cергей Иванович on 18.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
